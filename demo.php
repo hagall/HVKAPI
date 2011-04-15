79e61e3300accfec5da19a939e2a9cf1ff9439fa
Hagall <asbrandr@vataku.com>
2011-04-15 23:16:45 +0900

    first commit

--- demo.php
+++ demo.php
@@ -4,7 +4,7 @@
 	$res = $VK->login('email', 'pass');
 	if ($res['errcode']) die("Error code: {$res['errcode']}. {$res['errdesc']}");
 	
-	$resp = $VK->request('getProfiles', array('uids'=>'28818464,23363'));
+	$resp = $VK->request('getProfiles', array('uids'=>'1,23363'));
 	print_r($resp);
 ?>	
 
